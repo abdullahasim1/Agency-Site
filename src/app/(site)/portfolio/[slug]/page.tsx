c40dacf0a8893f48bae93e383aa3e24ce6c363d9
Abdullah Bin Asim <createfbid350@gmail.com>
2026-08-14 20:33:06 +0500

    Revert "Merge pull request #38 from abdullahasim1/narrative-layout"
    This reverts commit 9560f2401d996234bc2912504b9b3c4f68bf5402, reversing
    changes made to a8060045eb2768b067e284492216b7ce93dddc5c.

--- src/app/(site)/portfolio/[slug]/page.tsx
+++ src/app/(site)/portfolio/[slug]/page.tsx
@@ -1,7 +1,17 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-import { CaseStudyLayout } from "@/components/case-study/CaseStudyLayout";
+import { ChallengeSection } from "@/components/case-study/ChallengeSection";
+import { EngagementObjectives } from "@/components/case-study/EngagementObjectives";
+import { FeaturesGrid } from "@/components/case-study/FeaturesGrid";
+import { ProjectCTA } from "@/components/case-study/ProjectCTA";
+import { ProjectGallery } from "@/components/case-study/ProjectGallery";
+import { ProjectHero } from "@/components/case-study/ProjectHero";
+import { ProjectOverview } from "@/components/case-study/ProjectOverview";
+import { ResultsSection } from "@/components/case-study/ResultsSection";
+import { SolutionSection } from "@/components/case-study/SolutionSection";
+import { TechStack } from "@/components/case-study/TechStack";
+import { WorkflowDiagram } from "@/components/case-study/WorkflowDiagram";
 import { JsonLd } from "@/components/seo/JsonLd";
 import { getProjectBySlug, getProjectSlugs } from "@/data/projects";
 import { buildMetadata, caseStudySchema, pageGraph } from "@/lib/seo";
@@ -55,7 +65,17 @@ export default async function CaseStudyPage({ params }: CaseStudyPageProps) {
 
   return (
     <>
-      <CaseStudyLayout project={project} />
+      <ProjectHero project={project} />
+      <ProjectOverview project={project} />
+      <ChallengeSection project={project} />
+      <EngagementObjectives project={project} />
+      <SolutionSection project={project} />
+      <FeaturesGrid project={project} />
+      <TechStack project={project} />
+      <WorkflowDiagram project={project} />
+      <ResultsSection project={project} />
+      <ProjectGallery project={project} />
+      <ProjectCTA project={project} />
 
       <JsonLd
         data={pageGraph({
